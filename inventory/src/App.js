d3f783831193e197ee49372bc697713b3e0bdb26
Jazimnazir <jazim.marvelminds@gmail.com>
2023-11-28 10:45:16 +0530

    routing

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,8 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- inventory/src/pages/Home.js
+++ inventory/src/pages/Home.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import {
+  AppBar,
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  Rating,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static'>
+          <Toolbar>
+            <IconButton
+              size='large'
+              edge='start'
+              color='inherit'
+              aria-label='menu'
+              sx={{ mr: 2 }}
+            ></IconButton>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              Inventory
+            </Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
 };
 
 export default Home;
