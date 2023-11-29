8fb75b76504e19d8c7bcca7566c1cce76d480b9f
shahumer2 <shah.umer8493@gmail.com>
2023-11-29 11:11:34 +0530

    updated side bar and home

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Location } from './components/Location';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Location } from "./components/Location";
+import Sidebar from "./components/Sidebar.jsx";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/location/vessel' element={<Location />} />
-        </Routes>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/location/vessel" element={<Location />} />
+          </Routes>
+        </Sidebar>
       </BrowserRouter>
     </>
   );
--- inventory/src/components/Location.js
+++ inventory/src/components/Location.js
@@ -1,4 +1,4 @@
-import { Button } from '@mui/base';
+import { Button } from "@mui/base";
 import {
   AppBar,
   Card,
@@ -8,57 +8,15 @@ import {
   IconButton,
   Toolbar,
   Typography,
-} from '@mui/material';
-import { Box } from '@mui/system';
-import React from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const Location = () => {
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position='static' sx={{ backgroundColor: 'teal' }}>
-          <Toolbar>
-            <IconButton
-              size='large'
-              edge='start'
-              color='inherit'
-              aria-label='menu'
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-              Location/Vessel
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Grid>
-        <Card sx={{ minWidth: 275, pt: '11px' }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color='text.secondary'
-              gutterBottom
-            >
-              Word of the Day
-            </Typography>
-
-            <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-              adjective
-            </Typography>
-            <Typography variant='body2'>
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size='small'>Learn More</Button>
-          </CardActions>
-        </Card>
-      </Grid>
+      <div>hey</div>
     </>
   );
 };
