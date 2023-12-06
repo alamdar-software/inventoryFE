dfb4548c1800a125365d0637673e2dfc01f6f32d
Jazimnazir <jazim.marvelminds@gmail.com>
2023-12-06 12:06:28 +0530

    TransferItem

---
+++ inventory/src/components/Mto.js
@@ -0,0 +1,111 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+
+const Mto = () => {
+  return (
+    <>
+      <Grid>
+        <Card
+          color='secondary'
+          sx={{
+            width: '100%',
+            backgroundColor: 'secondary',
+            borderBottom: '2px solid yellow',
+            mb: '33px',
+          }}
+        >
+          <CardContent>
+            <Typography variant='h4' color='secondary' gutterBottom>
+              Transfer Mto
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid container spacing={2} sx={{ mt: '23px' }}>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth sx={{ width: '90%' }}>
+            <InputLabel id='demo-simple-select-label'>Location</InputLabel>
+            <Select
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              //value={age}
+              label='location'
+              //onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            sx={{ width: '90%' }}
+            id='outlined-basic'
+            label='Transfer Date'
+            variant='outlined'
+            // value={locationName}
+            // onChange={(e) => setLocation(e.target.value)}
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} sx={{ mt: '23px' }}>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth sx={{ width: '90%' }}>
+            <InputLabel id='demo-simple-select-label'>Consignee</InputLabel>
+            <Select
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              //value={age}
+              label='consignee'
+              //onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth sx={{ width: '90%' }}>
+            <InputLabel id='demo-simple-select-label'>
+              Repair/Service
+            </InputLabel>
+            <Select
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              //value={age}
+              label='Repair/service'
+              //onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: '33px' }}>
+        {' '}
+        <Button variant='contained' size='large' color='secondary'>
+          Add
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default Mto;
