994f93a1d278f80ea81d168c3494435ac343b0cf
shahumer2 <shah.umer8493@gmail.com>
2023-12-07 16:00:51 +0530

    inventory

--- inventory/src/components/Uom.js
+++ inventory/src/components/Uom.js
@@ -47,14 +47,8 @@ export const Uom = () => {
 
     getUom();
   }, []);
+  console.log(Uom, "uuuuuuuuuuuu");
 
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-  ];
   const handleClick = async () => {
     const res = await fetch("http://localhost:8080/unit/add", {
       method: "POST",
