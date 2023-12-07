95a7a0264c8844d93c78e4c123a83e1450074028
Jazimnazir <jazim.marvelminds@gmail.com>
2023-12-07 16:01:30 +0530

    item

--- inventory/src/components/Item.js
+++ inventory/src/components/Item.js
@@ -1,28 +1,28 @@
-import { FormControl, Grid, InputLabel, MenuItem, Select } from "@mui/material";
-import React from "react";
+import { FormControl, Grid, InputLabel, MenuItem, Select } from '@mui/material';
+import React from 'react';
 import {
   Card,
   CardContent,
   TextField,
   Button,
   Typography,
-} from "@mui/material";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { fetchCategory } from "../redux/slice/CategorySlice";
-import { useEffect } from "react";
-import { fetchUom } from "../redux/slice/UomSlice";
-import { useState } from "react";
+} from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { fetchCategory } from '../redux/slice/CategorySlice';
+import { useEffect } from 'react';
+import { fetchUom } from '../redux/slice/UomSlice';
+import { useState } from 'react';
 
 const Item = () => {
   const [formData, setformData] = useState({
-    itemName: "",
-    minimumStock: "",
-    description: "",
-    name: "",
-    unitName: "",
+    itemName: '',
+    minimumStock: '',
+    description: '',
+    name: '',
+    unitName: '',
   });
-  console.log(formData, "formmmmmmmmm");
+  console.log(formData, 'formmmmmmmmm');
   const state = useSelector((state) => state);
 
   const dispatch = useDispatch();
@@ -37,15 +37,15 @@ const Item = () => {
       [e.target.id]: e.target.value,
     });
   };
-  console.log(formData, "heyyyy");
+  console.log(formData, 'heyyyy');
 
   const handleClick = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:8080/item/add", {
-        method: "post",
+      const res = await fetch('http://localhost:8080/item/add', {
+        method: 'post',
         headers: {
-          "content-type": "application/json",
+          'content-type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
@@ -58,15 +58,15 @@ const Item = () => {
     <>
       <Grid>
         <Card
-          color="secondary"
+          color='secondary'
           sx={{
-            width: "100%",
-            backgroundColor: "secondary",
-            borderBottom: "2px solid yellow",
+            width: '100%',
+            backgroundColor: 'secondary',
+            borderBottom: '2px solid yellow',
           }}
         >
           <CardContent>
-            <Typography variant="h4" color="secondary" gutterBottom>
+            <Typography variant='h4' color='secondary' gutterBottom>
               Add Item
             </Typography>
           </CardContent>
@@ -75,50 +75,50 @@ const Item = () => {
 
       <Card
         sx={{
-          width: "100%",
-          mt: "33px",
-          pt: "33px",
-          borderBottom: "2px solid yellow",
-          borderRadius: "33px",
+          width: '100%',
+          mt: '33px',
+          pt: '33px',
+          borderBottom: '2px solid yellow',
+          borderRadius: '33px',
         }}
       >
-        <Grid container spacing={2} sx={{ ml: "13px" }}>
+        <Grid container spacing={2} sx={{ ml: '13px' }}>
           <Grid item xs={12} sm={6}>
             <TextField
-              id="itemName"
-              label="Item"
-              variant="outlined"
+              id='itemName'
+              label='Item'
+              variant='outlined'
               onChange={handleInputChange}
               //   value={location}
               //   onChange={(e) => setLocation(e.target.value)}
               fullWidth
-              sx={{ width: "90%" }}
+              sx={{ width: '90%' }}
             />
           </Grid>
           <Grid item xs={12} sm={6}>
             <TextField
-              id="description"
-              label="Item Description"
-              variant="outlined"
+              id='description'
+              label='Item Description'
+              variant='outlined'
               onChange={handleInputChange}
               //   value={subLocation}
               //   onChange={(e) => setSubLocation(e.target.value)}
               fullWidth
-              sx={{ width: "90%" }}
+              sx={{ width: '90%' }}
             />
           </Grid>
         </Grid>
 
-        <Grid container spacing={2} sx={{ ml: "13px", mt: "21px" }}>
+        <Grid container spacing={2} sx={{ ml: '13px', mt: '21px' }}>
           <Grid item xs={12} sm={6}>
-            <FormControl fullWidth sx={{ width: "90%" }}>
-              <InputLabel id="Catagory">Catagory</InputLabel>
+            <FormControl fullWidth sx={{ width: '90%' }}>
+              <InputLabel id='Catagory'>Catagory</InputLabel>
               <Select
-                labelId="Catagory"
-                id="name"
+                labelId='Catagory'
+                id='name'
                 value={formData?.name}
                 //value={age}
-                label="Catagory"
+                label='Catagory'
                 onChange={(e) =>
                   setformData({
                     ...formData,
@@ -129,7 +129,7 @@ const Item = () => {
               >
                 {state.category.data?.content.map((item, index) => (
                   <MenuItem key={index} value={item?.name}>
-                    {" "}
+                    {' '}
                     {item?.name}
                   </MenuItem>
                 ))}
@@ -137,14 +137,14 @@ const Item = () => {
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <FormControl fullWidth sx={{ width: "90%" }}>
-              <InputLabel id="demo-simple-select-label">UOM</InputLabel>
+            <FormControl fullWidth sx={{ width: '90%' }}>
+              <InputLabel id='demo-simple-select-label'>UOM</InputLabel>
               <Select
-                labelId="demo-simple-select-label"
-                id="unitName"
+                labelId='demo-simple-select-label'
+                id='unitName'
                 value={formData.unitName}
                 //value={age}
-                label="UOM"
+                label='UOM'
                 onChange={(e) =>
                   setformData({
                     ...formData,
@@ -155,7 +155,7 @@ const Item = () => {
               >
                 {state.Uom.data?.content.map((item, index) => (
                   <MenuItem key={index} value={item?.unitName}>
-                    {" "}
+                    {' '}
                     {item?.unitName}
                   </MenuItem>
                 ))}
@@ -163,32 +163,32 @@ const Item = () => {
             </FormControl>
           </Grid>
         </Grid>
-        <Grid container spacing={2} sx={{ mt: "21px", ml: "13px" }}>
+        <Grid container spacing={2} sx={{ mt: '21px', ml: '13px' }}>
           <Grid item xs={12} sm={6}>
             <TextField
-              id="minimumStock"
-              label="Minimum Stock"
-              variant="outlined"
+              id='minimumStock'
+              label='Minimum Stock'
+              variant='outlined'
               //   value={subLocation}
               onChange={handleInputChange}
               //   onChange={(e) => setSubLocation(e.target.value)}
               fullWidth
-              sx={{ width: "90%" }}
+              sx={{ width: '90%' }}
             />
           </Grid>
         </Grid>
         <Button
-          variant="contained"
-          color="secondary"
-          size="large"
+          variant='contained'
+          color='secondary'
+          size='large'
           onClick={handleClick}
           //onClick={handleClick}
           sx={{
-            mt: "33px",
-            mb: "17px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            display: "block",
+            mt: '33px',
+            mb: '17px',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            display: 'block',
           }}
         >
           Add
