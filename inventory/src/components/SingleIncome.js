3b2c6868c836fa86d2b2e1c442dcb40d8b5b40ac
Jazimnazir <jazim.marvelminds@gmail.com>
2023-12-14 09:47:10 +0530

    changes

--- inventory/src/components/SingleIncome.js
+++ inventory/src/components/SingleIncome.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import {
   Button,
@@ -11,24 +11,24 @@ import {
   Select,
   TextField,
   Typography,
-} from "@mui/material";
+} from '@mui/material';
 
 const SingleIncome = () => {
   return (
     <>
       <Grid>
         <Card
-          color="secondary"
+          color='secondary'
           sx={{
-            width: "100%",
-            backgroundColor: "secondary",
-            borderBottom: "2px solid yellow",
+            width: '100%',
+            backgroundColor: 'secondary',
+            borderBottom: '2px solid yellow',
           }}
         >
           <CardContent>
             <Typography
-              variant="h4"
-              color="secondary"
+              variant='h4'
+              color='secondary'
               gutterBottom
               style={{ fontFamily: "'EB Garamond'" }}
             >
@@ -38,15 +38,15 @@ const SingleIncome = () => {
         </Card>
       </Grid>
 
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
-          <FormControl fullWidth sx={{ width: "90%" }}>
-            <InputLabel id="demo-simple-select-label">Location</InputLabel>
+          <FormControl fullWidth sx={{ width: '90%' }}>
+            <InputLabel id='demo-simple-select-label'>Location</InputLabel>
             <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
               //value={age}
-              label="location"
+              label='location'
               //onChange={handleChange}
             >
               <MenuItem value={10}>Ten</MenuItem>
@@ -56,13 +56,13 @@ const SingleIncome = () => {
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <FormControl fullWidth sx={{ width: "90%" }}>
-            <InputLabel id="demo-simple-select-label">Sub Location</InputLabel>
+          <FormControl fullWidth sx={{ width: '90%' }}>
+            <InputLabel id='demo-simple-select-label'>Sub Location</InputLabel>
             <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
               //value={age}
-              label="sublocation"
+              label='sublocation'
               //onChange={handleChange}
             >
               <MenuItem value={10}>Ten</MenuItem>
@@ -73,17 +73,17 @@ const SingleIncome = () => {
         </Grid>
       </Grid>
 
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
-          <FormControl fullWidth sx={{ width: "90%" }}>
-            <InputLabel id="demo-simple-select-label">
+          <FormControl fullWidth sx={{ width: '90%' }}>
+            <InputLabel id='demo-simple-select-label'>
               Item Description
             </InputLabel>
             <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
               //value={age}
-              label="location"
+              label='location'
               //onChange={handleChange}
             >
               <MenuItem value={10}>Ten</MenuItem>
@@ -94,25 +94,25 @@ const SingleIncome = () => {
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Catagory"
-            variant="outlined"
+            id='outlined-basic'
+            label='Catagory'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
             InputProps={{
               readOnly: true,
             }}
           />
         </Grid>
       </Grid>
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Unit of Measure"
-            variant="outlined"
+            id='outlined-basic'
+            label='Unit of Measure'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
             InputProps={{
               readOnly: true,
             }}
@@ -120,45 +120,45 @@ const SingleIncome = () => {
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Extended Value"
-            variant="outlined"
+            id='outlined-basic'
+            label='Extended Value'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
       </Grid>
 
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Quantity"
-            variant="outlined"
-            type="number"
+            id='outlined-basic'
+            label='Quantity'
+            variant='outlined'
+            type='number'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Unit Cost"
-            variant="outlined"
+            id='outlined-basic'
+            label='Unit Cost'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
       </Grid>
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Total Price"
-            variant="outlined"
-            type="number"
+            id='outlined-basic'
+            label='Total Price'
+            variant='outlined'
+            type='number'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
             InputProps={{
               readOnly: true,
             }}
@@ -166,84 +166,84 @@ const SingleIncome = () => {
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Standard Price"
-            variant="outlined"
+            id='outlined-basic'
+            label='Standard Price'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
       </Grid>
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Purchase Date"
-            variant="outlined"
-            type="number"
+            id='outlined-basic'
+            label='Purchase Date'
+            variant='outlined'
+            type='number'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Purchase Order"
-            variant="outlined"
+            id='outlined-basic'
+            label='Purchase Order'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
       </Grid>
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="P/N"
-            variant="outlined"
+            id='outlined-basic'
+            label='P/N'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="S/N"
-            variant="outlined"
+            id='outlined-basic'
+            label='S/N'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
       </Grid>
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Brand"
-            variant="outlined"
+            id='outlined-basic'
+            label='Brand'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Currency"
-            variant="outlined"
+            id='outlined-basic'
+            label='Currency'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
       </Grid>
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
-          <FormControl fullWidth sx={{ width: "90%" }}>
-            <InputLabel id="demo-simple-select-label">Entity</InputLabel>
+          <FormControl fullWidth sx={{ width: '90%' }}>
+            <InputLabel id='demo-simple-select-label'>Entity</InputLabel>
             <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
               //value={age}
-              label="location"
+              label='location'
               //onChange={handleChange}
             >
               <MenuItem value={10}>Ten</MenuItem>
@@ -254,46 +254,46 @@ const SingleIncome = () => {
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="IMPA Code"
-            variant="outlined"
+            id='outlined-basic'
+            label='IMPA Code'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
       </Grid>
-      <Grid container spacing={2} sx={{ mt: "33px" }}>
+      <Grid container spacing={2} sx={{ mt: '33px' }}>
         <Grid item xs={21} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Store Number"
-            variant="outlined"
+            id='outlined-basic'
+            label='Store Number'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="outlined-basic"
-            label="Remarks"
-            variant="outlined"
+            id='outlined-basic'
+            label='Remarks'
+            variant='outlined'
             fullWidth
-            sx={{ width: "90%" }}
+            sx={{ width: '90%' }}
           />
         </Grid>
       </Grid>
       <Button
-        variant="contained"
-        color="secondary"
-        size="large"
+        variant='contained'
+        color='secondary'
+        size='large'
         //onClick={handleClick}
 
         sx={{
-          mt: "33px",
-          mb: "17px",
-          marginLeft: "auto",
-          marginRight: "auto",
-          display: "block",
+          mt: '33px',
+          mb: '17px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          display: 'block',
         }}
       >
         Add
