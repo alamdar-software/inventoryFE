afcd4dfe24bae6958757503e748ec562aaf0abf8
shahumer2 <shah.umer8493@gmail.com>
2024-02-09 16:17:04 +0530

    test

--- inventory/src/components/PrivateRoute.jsx
+++ inventory/src/components/PrivateRoute.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = () => {
   const { currentUser } = useSelector((state) => state.persisted.user);
+  const location = useLocation();
+  useEffect(() => {
+    if (!currentUser) {
+      window.location.reload();
+    }
+  }, [currentUser]);
 
   return currentUser ? <Outlet /> : <Navigate to="/login" />;
+  window.location.reload();
 };
 
 export default PrivateRoute;
