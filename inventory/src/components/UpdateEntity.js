c61fb408068d2231ef55d1d892c28fef7ab483ef
Jazimnazir <jazim.marvelminds@gmail.com>
2024-02-14 10:35:51 +0530

    Entity Changes on security

--- inventory/src/components/UpdateEntity.js
+++ inventory/src/components/UpdateEntity.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Card,
   CardContent,
@@ -6,11 +6,12 @@ import {
   TextField,
   Button,
   Typography,
-} from "@mui/material";
+} from '@mui/material';
 
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 // const currency = {
 //   currencyName: "",
 // };
@@ -18,21 +19,27 @@ import { useNavigate } from "react-router-dom";
 export default function UpdateEntity() {
   const navigate = useNavigate();
   const [entity, setEntity] = useState({
-    entityName: "",
+    entityName: '',
   });
   const [Loading, setLoading] = useState(false);
   const [error, seterror] = useState(false);
   const [data, setdata] = useState([]);
+  const state = useSelector((state) => state);
+  const { currentUser } = state.persisted.user;
   const { id } = useParams();
   console.log(entity);
 
   useEffect(() => {
     const getEntity = async () => {
-      const res = await fetch(`http://localhost:8080/entity/get/${id}`);
+      const res = await fetch(`http://localhost:8080/entity/get/${id}`, {
+        headers: {
+          Authorization: `Bearer ${currentUser.accessToken}`,
+        },
+      });
 
       const data = await res.json();
 
-      console.log(data, "backdata");
+      console.log(data, 'backdata');
       setEntity({
         entityName: data.entityName,
       });
@@ -42,16 +49,17 @@ export default function UpdateEntity() {
   }, []);
   const handleClick = async () => {
     const res = await fetch(`http://localhost:8080/entity/update/${id}`, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${currentUser.accessToken}`,
       },
 
       body: JSON.stringify(entity),
     });
     const data = await res.text();
     console.log(data);
-    navigate("/entity");
+    navigate('/entity');
 
     // fetch('http://localhost:8080/location/add', {
     //   method: 'POST',
@@ -70,11 +78,11 @@ export default function UpdateEntity() {
     <>
       <Grid>
         <Card
-          color="secondary"
-          sx={{ width: "100%", backgroundColor: "secondary" }}
+          color='secondary'
+          sx={{ width: '100%', backgroundColor: 'secondary' }}
         >
           <CardContent>
-            <Typography variant="h4" color="secondary" gutterBottom>
+            <Typography variant='h4' color='secondary' gutterBottom>
               Update Entity
             </Typography>
           </CardContent>
@@ -83,21 +91,21 @@ export default function UpdateEntity() {
 
       <Card
         sx={{
-          width: "100%",
-          mt: "33px",
-          pt: "33px",
-          borderBottom: "2px solid grey",
+          width: '100%',
+          mt: '33px',
+          pt: '33px',
+          borderBottom: '2px solid grey',
         }}
       >
         <Grid
           container
           spacing={2}
-          sx={{ ml: "11px", justifyContent: "center" }}
+          sx={{ ml: '11px', justifyContent: 'center' }}
         >
           <Grid item xs={12} sm={6}>
             <TextField
-              name="currencyName"
-              variant="outlined"
+              name='currencyName'
+              variant='outlined'
               value={entity?.entityName}
               /*  value={currency?.currencyName} */
               onChange={(e) =>
@@ -110,16 +118,16 @@ export default function UpdateEntity() {
           </Grid>
         </Grid>
         <Button
-          variant="contained"
-          color="secondary"
-          size="large"
+          variant='contained'
+          color='secondary'
+          size='large'
           onClick={handleClick}
           sx={{
-            mt: "33px",
-            mb: "17px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            display: "block",
+            mt: '33px',
+            mb: '17px',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            display: 'block',
           }}
         >
           Update
