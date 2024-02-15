147f8e4994444748283209637fce65ab980ea8cf
Jazimnazir <jazim.marvelminds@gmail.com>
2024-02-15 14:27:11 +0530

    changes on mto ,it

--- inventory/src/components/InternalTransfer.js
+++ inventory/src/components/InternalTransfer.js
@@ -94,7 +94,7 @@ const InternalTransfer = () => {
       locationName: selectedLocation,
       // Reset sublocation when location changes
     });
-    const selectedLocationObj = state.location.data.find(
+    const selectedLocationObj = state.nonPersisted.location.data.find(
       (location) => location.locationName === selectedLocation
     );
     setSubLocations(selectedLocationObj ? selectedLocationObj?.addresses : []);
--- inventory/src/components/UpdateUser.js
+++ inventory/src/components/UpdateUser.js
@@ -1,38 +1,38 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { InputLabel, MenuItem, Select } from "@mui/material";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { InputLabel, MenuItem, Select } from '@mui/material';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function Copyright(props) {
   return (
     <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
+      variant='body2'
+      color='text.secondary'
+      align='center'
       {...props}
     >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
         Your Website
-      </Link>{" "}
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
   );
 }
@@ -45,30 +45,30 @@ export default function UpdateUser() {
   const { id } = useParams();
   const { currentUser } = useSelector((state) => state.persisted.user);
 
-  const [selectedRole, setSelectedRole] = useState("");
+  const [selectedRole, setSelectedRole] = useState('');
   const navigate = useNavigate();
 
   const [formData, setformData] = useState({});
   const [user, setuser] = useState({
-    name: "",
-    username: "",
-    email: "",
-    contactNumber: "",
-    roles: "",
+    name: '',
+    username: '',
+    email: '',
+    contactNumber: '',
+    roles: '',
     // Add other properties as needed
   });
-  const [role, setrole] = useState("");
+  const [role, setrole] = useState('');
   useEffect(() => {
     const getUser = async () => {
       const res = await fetch(`http://localhost:8080/api/user/getById/${id}`, {
-        method: "get",
+        method: 'get',
         headers: {
           Authorization: `Bearer ${currentUser.accessToken}`,
         },
       });
       const data = await res.json();
       setuser(data);
-      console.log(data, "user by id");
+      console.log(data, 'user by id');
     };
     getUser();
   }, []);
@@ -80,19 +80,19 @@ export default function UpdateUser() {
       const userData = { name, username, email, roles }; // Create new object with specific fields
 
       const res = await fetch(`http://localhost:8080/api/user/update/${id}`, {
-        method: "PUT",
+        method: 'PUT',
         headers: {
           Authorization: `Bearer ${currentUser?.accessToken}`,
-          "Content-Type": "application/json", // Set content type to JSON
+          'Content-Type': 'application/json', // Set content type to JSON
         },
         body: JSON.stringify(userData), // Stringify the new object with specific fields
       });
 
-      console.log(userData, "user data sent");
+      console.log(userData, 'user data sent');
       const data = await res.json();
-      console.log(data, "response data");
+      console.log(data, 'response data');
       if (res.ok) {
-        navigate("/dashboard");
+        navigate('/dashboard');
       }
     } catch (error) {
       console.log(error.message);
@@ -104,15 +104,15 @@ export default function UpdateUser() {
     roleName = user.roles[0].name;
   } else {
     // handle the case where roleName cannot be determined
-    roleName = "Unknown";
+    roleName = 'Unknown';
   }
   const handleChange = (event) => {
     const { id, value } = event.target;
-    console.log("Selected ID:", id); // Check the selected ID
-    console.log("Selected Value:", value); // Check the selected value
-    const parsedValue = id === "contactNumber" ? parseInt(value) : value;
+    console.log('Selected ID:', id); // Check the selected ID
+    console.log('Selected Value:', value); // Check the selected value
+    const parsedValue = id === 'contactNumber' ? parseInt(value) : value;
 
-    if (id === "roles") {
+    if (id === 'roles') {
       // Find the selected role object in the roles array
       const selectedRole = user.roles.find((role) => role.name === value);
       // If the selected role is found, update the user state with its name
@@ -122,7 +122,7 @@ export default function UpdateUser() {
           [id]: parsedValue,
           roles: selectedRole.name,
         });
-        console.log("i am in first loggg");
+        console.log('i am in first loggg');
       } else {
         // Handle the case when the selected role is not found
         console.error(`Role ${value} not found in the user roles array`);
@@ -135,109 +135,109 @@ export default function UpdateUser() {
       });
     }
 
-    console.log("User Data:", user); // Log the updated user data
+    console.log('User Data:', user); // Log the updated user data
   };
 
-  console.log(roleName, "nama");
+  console.log(roleName, 'nama');
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
+      <Container component='main' maxWidth='xs'>
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component='h1' variant='h5'>
             Update User
           </Typography>
           <Box
-            component="form"
+            component='form'
             noValidate
             onSubmit={handleSubmit}
             sx={{ mt: 3 }}
           >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
-                <InputLabel htmlFor="name">Enter Name</InputLabel>
+                <InputLabel htmlFor='name'>Enter Name</InputLabel>
                 <TextField
-                  autoComplete="given-name"
-                  name="name"
+                  autoComplete='given-name'
+                  name='name'
                   required
                   fullWidth
-                  id="name"
+                  id='name'
                   value={user.name}
                   autoFocus
                   onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <InputLabel htmlFor="username">Enter Username</InputLabel>
+                <InputLabel htmlFor='username'>Enter Username</InputLabel>
                 <TextField
                   required
                   fullWidth
-                  id="username"
-                  name="username"
+                  id='username'
+                  name='username'
                   value={user.username}
-                  autoComplete="username"
+                  autoComplete='username'
                   onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
-                <InputLabel htmlFor="email">Enter Email</InputLabel>
+                <InputLabel htmlFor='email'>Enter Email</InputLabel>
                 <TextField
                   required
                   fullWidth
-                  id="email"
+                  id='email'
                   value={user.email}
-                  name="email"
-                  autoComplete="email"
+                  name='email'
+                  autoComplete='email'
                   onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
-                <InputLabel htmlFor="contactNumber">Contact Number</InputLabel>
+                <InputLabel htmlFor='contactNumber'>Contact Number</InputLabel>
                 <TextField
-                  type="number"
+                  type='number'
                   required
                   fullWidth
-                  id="contactNumber"
+                  id='contactNumber'
                   value={user.contactNumber}
-                  name="contactNumber"
-                  autoComplete="contactNumber"
+                  name='contactNumber'
+                  autoComplete='contactNumber'
                   onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
-                <InputLabel htmlFor="roles">Select Role</InputLabel>
+                <InputLabel htmlFor='roles'>Select Role</InputLabel>
                 <Select
-                  id="roles"
+                  id='roles'
                   fullWidth
-                  label="Select Role"
-                  placeholder="Select Role"
-                  value={roleName || ""}
+                  label='Select Role'
+                  placeholder='Select Role'
+                  value={roleName || ''}
                   onChange={(e) => {
                     setuser({ ...user, roles: e.target.value }); // Update the user's role
                   }}
                 >
-                  <MenuItem value="ROLE_SUPERADMIN">ROLE_SUPERADMIN</MenuItem>
-                  <MenuItem value="ROLE_VERIFIER">ROLE_VERIFIER</MenuItem>
-                  <MenuItem value="ROLE_PREPARER">ROLE_PREPARER</MenuItem>
-                  <MenuItem value="ROLE_APPROVER">ROLE_APPROVER</MenuItem>
+                  <MenuItem value='ROLE_SUPERADMIN'>ROLE_SUPERADMIN</MenuItem>
+                  <MenuItem value='ROLE_VERIFIER'>ROLE_VERIFIER</MenuItem>
+                  <MenuItem value='ROLE_PREPARER'>ROLE_PREPARER</MenuItem>
+                  <MenuItem value='ROLE_APPROVER'>ROLE_APPROVER</MenuItem>
                 </Select>
               </Grid>
             </Grid>
             <Button
-              type="submit"
+              type='submit'
               fullWidth
-              variant="contained"
+              variant='contained'
               sx={{ mt: 3, mb: 2 }}
               onClick={handleSubmit}
             >
