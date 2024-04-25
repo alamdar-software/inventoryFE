64e53e2821d99e33e53351d45d273d1716fa9891
Jazim Nazir <109747177+Jazimnazir@users.noreply.github.com>
2024-04-25 15:20:23 +0530

    pagination changes

--- inventory/src/pages/ViewCipl.js
+++ inventory/src/pages/ViewCipl.js
@@ -17,7 +17,7 @@ import {
   Paper,
   Table,
   TableBody,
-  TablePagination,
+  
   Box,
   TableFooter,
   IconButton,
@@ -32,6 +32,17 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import EditIcon from '@mui/icons-material/Edit';
+import {
+  TablePagination,
+  tablePaginationClasses as classes,
+} from '@mui/base/TablePagination';
+import { styled } from '@mui/system';
+import FirstPageRoundedIcon from '@mui/icons-material/FirstPageRounded';
+import LastPageRoundedIcon from '@mui/icons-material/LastPageRounded';
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+
+
 export const ViewCipl = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
@@ -42,6 +53,8 @@ export const ViewCipl = () => {
   const [allCipl, setAllCipl] = useState([]);
   const [filteredCipl, setFilteredCipl] = useState([]);
   const { currentUser } = useSelector((state) => state.persisted.user);
+  const [totalRows,setTotalRows] = useState(0);
+  
 
   useEffect(() => {
     dispatch(fetchlocation(currentUser.accessToken));
@@ -144,6 +157,12 @@ export const ViewCipl = () => {
       });
   };
   useEffect(() => {
+    // Update totalRows when locationData or itemData changes
+    
+      setTotalRows(filteredCipl.length);
+    
+  },);
+  useEffect(() => {
     fetch('http://localhost:8080/cipl/approved', {
       method: 'GET',
       headers: {
@@ -161,6 +180,7 @@ export const ViewCipl = () => {
         console.log(result, 'rupaaaaaaaaaaa');
         setAllCipl(result);
         setFilteredCipl(result);
+       
       })
       .catch((error) => {
         console.error('Error fetching pickup data:', error);
@@ -460,27 +480,31 @@ export const ViewCipl = () => {
             </TableBody>
 
             <TableFooter>
-              <TableRow>
-                <TableCell colSpan={7} align='center'>
-                  <hr style={{ width: '100%', marginLeft: '100px' }} />
-                  <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]}
-                    component='div'
-                    count={filteredCipl.length}
-                    rowsPerPage={rowsPerPage}
+            <TableRow>
+                <TableCell colSpan={5} align="center">
+                  <CustomTablePagination
+                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                    colSpan={3}
+                    count={totalRows}
+                    rowsPerPage={5}
                     page={page}
+                    slotProps={{
+                      select: {
+                        'aria-label': 'Rows per page',
+                      },
+                      actions: {
+                        showFirstButton: true,
+                        showLastButton: true,
+                        slots: {
+                          firstPageIcon: FirstPageRoundedIcon,
+                          lastPageIcon: LastPageRoundedIcon,
+                          nextPageIcon: ChevronRightRoundedIcon,
+                          backPageIcon: ChevronLeftRoundedIcon,
+                        },
+                      },
+                    }}
                     onPageChange={handleChangePage}
                     onRowsPerPageChange={handleChangeRowsPerPage}
-                    style={{ fontWeight: 'bolder' }}
-                    labelRowsPerPage={
-                      <span
-                        style={{
-                          fontWeight: 'bold',
-                        }}
-                      >
-                        Rows per page:
-                      </span>
-                    }
                   />
                 </TableCell>
               </TableRow>
@@ -491,5 +515,144 @@ export const ViewCipl = () => {
     </>
   );
 };
+
+
+const blue = {
+  200: '#A5D8FF',
+  400: '#3399FF',
+};
+
+const grey = {
+  50: '#F3F6F9',
+  100: '#E5EAF2',
+  200: '#DAE2ED',
+  300: '#C7D0DD',
+  400: '#B0B8C4',
+  500: '#9DA8B7',
+  600: '#6B7A90',
+  700: '#434D5B',
+  800: '#303740',
+  900: '#1C2025',
+};
+
+const Root = styled('div')(
+  ({ theme }) => `
+  table {
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 0.875rem;
+    width: 100%;
+    background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+    box-shadow: 0px 4px 16px ${
+      theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.3)' : grey[200]
+    };
+    border-radius: 12px;
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+    overflow: hidden;
+  }
+
+  td,
+  th {
+    padding: 16px;
+  }
+
+  th {
+    background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  }
+  `,
+);
+
+const CustomTablePagination = styled(TablePagination)(
+  ({ theme }) => `
+  & .${classes.spacer} {
+    display: none;
+  }
+
+  & .${classes.toolbar}  {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
+    background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+
+  & .${classes.selectLabel} {
+    margin: 0;
+  }
+
+  & .${classes.select}{
+    font-family: 'IBM Plex Sans', sans-serif;
+    padding: 2px 0 2px 4px;
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+    border-radius: 6px; 
+    background-color: transparent;
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    transition: all 100ms ease;
+
+    &:hover {
+      background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
+      border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
+    }
+
+    &:focus {
+      outline: 3px solid ${theme.palette.mode === 'dark' ? blue[400] : blue[200]};
+      border-color: ${blue[400]};
+    }
+  }
+
+  & .${classes.displayedRows} {
+    margin: 0;
+
+    @media (min-width: 768px) {
+      margin-left: auto;
+    }
+  }
+
+  & .${classes.actions} {
+    display: flex;
+    gap: 6px;
+    border: transparent;
+    text-align: center;
+  }
+
+  & .${classes.actions} > button {
+    display: flex;
+    align-items: center;
+    padding: 0;
+    border: transparent;
+    border-radius: 50%; 
+    background-color: transparent;
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    transition: all 100ms ease;
+
+    > svg {
+      font-size: 22px;
+    }
+
+    &:hover {
+      background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
+      border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
+    }
+
+    &:focus {
+      outline: 3px solid ${theme.palette.mode === 'dark' ? blue[400] : blue[200]};
+      border-color: ${blue[400]};
+    }
+
+    &:disabled {
+      opacity: 0.3;
+      &:hover {
+        border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+        background-color: transparent;
+      }
+    }
+  }
+  `,
+);
 
 export default ViewCipl;
