26535de1647f0af9dd02c596c31eb392c4ef8fb1
UMER MUKHTAR <shah.umer8493@gmail.com>
2024-08-14 16:16:05 +0530

    update

--- inventory/src/components/UpdateCiplVerifier.js
+++ inventory/src/components/UpdateCiplVerifier.js
@@ -969,7 +969,7 @@ const UpdateCiplVerifier = () => {
            
             >
               <MenuItem value={'verified'}>Verified</MenuItem>
-              <MenuItem value={'verifier rejected'}>Rejected</MenuItem>
+              <MenuItem value={'verifierRejected'}>Rejected</MenuItem>
             </Select>
           </FormControl>
         </Grid>
